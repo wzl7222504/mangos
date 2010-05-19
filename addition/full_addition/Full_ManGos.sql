135488266a0780b5896d0e9b8a4ee3197bc88b15
Salja <beleko2009@web.de>
2010-05-19 16:33:41 +0200

    [Patch 023] Typo in SQL [Patch 015].
    Signed-off-by: Salja <beleko2009@web.de>

--- addition/full_addition/Full_ManGos.sql
+++ addition/full_addition/Full_ManGos.sql
@@ -43,7 +43,7 @@ INSERT INTO `spell_proc_event` VALUES
 -- 15_ManGos_String_ManGos
 
 DELETE FROM `mangos_string` WHERE `entry` = 11000;
-INSERT INTO `mangos_string` VALUES (11000, '|cffff0000[Anticheat]: Cheater detected. Nickname: %s. Cheat type: %s.|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '|cffff0000[???????]: ????? ?????????. ???: %s. ??? ????: %s.|r');
+INSERT INTO `mangos_string` VALUES (11000, '|cffff0000[Anticheat]: Cheater Detected. Nickname: %s. Cheat Type: %s.|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '|cffff0000[???????]: ????? ?????????. ???: %s. ??? ????: %s.|r');
 
 -- 20_Spell_Proc_Event_ManGos
 
